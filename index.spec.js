c03d7501cc25e83bf46dc2b82e6b9b3bfe13dab1
Michael Rubanov <persorub@personalics.com>
2019-09-11 00:44:44 +0300

    @tested-base

---
+++ index.spec.js
@@ -0,0 +1,9 @@
+describe('base test suite', function() {
+  it('true should be thruty', function() {
+    expect(true).to.be.true;
+  });
+  it('false should be falsy', function() {
+    expect(false).to.be.false;
+  });
+});
+
